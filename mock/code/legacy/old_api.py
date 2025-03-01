d023fd3c12b484b73ccc76fff5ed0fd40d526dfa
Chris McKenzie <kristopolous@yahoo.com>
2025-03-01 15:09:23 -0800

    updates

--- mock/code/legacy/old_api.py
+++ mock/code/legacy/old_api.py
@@ -4,4 +4,4 @@ Written by Bob (short tenure, non-expert).
 """
 
 def legacy_function():
-    print("This function is deprecated and should not be used.")
+    print("This function is deprecated and should not be used. Please use the new API.")
